f3ed93110b265beaaf0516f788cede27d1c3db55
jlopep09 <jlopep09@estudiantes.unileon.es>
2025-05-08 14:07:50 +0200

    Updated DDBB scripts

--- ui/src/main.jsx
+++ ui/src/main.jsx
@@ -8,7 +8,7 @@ createRoot(document.getElementById('root')).render(
       <BrowserRouter>
             <Routes >
                   <Route path="/" element={<App />} />
-                  <Route path="profile" element={<Profile UserID={3}/>} />
+                  <Route path="profile" element={<Profile UserID={4}/>} />
                   <Route path="login" element={<App />} />
                   <Route path="register" element={<App/>} />
             </Routes>
