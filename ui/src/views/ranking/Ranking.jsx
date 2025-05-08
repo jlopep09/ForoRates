61e7fed0d430d0d5458626d62e9deae6e6ddeb8c
jlopep09 <jlopep09@estudiantes.unileon.es>
2025-05-08 15:12:35 +0200

    implemented ranking fetch

--- ui/src/views/ranking/Ranking.jsx
+++ ui/src/views/ranking/Ranking.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { RankingTable } from './components/RankingTable';
 import Navbar from '../../components/Navbar';
+import { ENDPOINTS } from '../../../constants';
 
 const darkTheme = createTheme({
   palette: {
@@ -10,7 +11,25 @@ const darkTheme = createTheme({
   },
 });
 export const Ranking = () => {
-  
+    //listado de usuarios ordenado por score
+    const [userData, setUserData] = useState(null);
+
+    useEffect(() => {
+        async function fetchUserData() {
+          try {
+            const response = await fetch(`${ENDPOINTS.RANKING}`);
+            if (!response.ok) {
+              throw new Error("Error fetching ranking data");
+            }
+            const data = await response.json();
+            setUserData(data);
+          } catch (error) {
+            console.error("Error fetching ranking data:", error);
+          }
+        }
+    
+        fetchUserData();
+      }, []);
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
@@ -19,9 +38,16 @@ export const Ranking = () => {
         <div className='flex flex-col align-middle justify-start py-10 items-center min-h-100'>
             <h1 className='uppercase font-bold text-2xl'>Best users</h1>
             <hr className='mx-6 w-100 mb-2'/>
-            <RankingTable>
-
-            </RankingTable>
+            {
+                userData &&(
+                    <RankingTable usersData={userData}/>
+                )
+            }
+            {
+                !userData &&(
+                    <p className='font-bold text-blue-300'>The user ranking is comming soon!</p>
+                )
+            }
         </div>
       </main>
     </ThemeProvider>
--- ui/src/views/ranking/components/RankingTable.jsx
+++ ui/src/views/ranking/components/RankingTable.jsx
@@ -1,22 +1,16 @@
 import { Button, Paper } from '@mui/material';
 import React from 'react'
 
-export const RankingTable = () => {
+export const RankingTable = ({usersData}) => {
 
-    const users = [
-        {username: "jose", score: 99},
-        {username: "felipe", score: 10},
-        {username: "manuel", score: 13},
-        {username: "paconi", score: 35},
-        {username: "ancheloti", score: 75}
-    ];
   return (
     <>  
     <Paper elevation={2} className='mb-5 py-3 flex flex-col gap-3 justify-center align-middle items-center'>
     <RankingRow username={"User"} score={"Score"}></RankingRow>
     <hr className='w-80' />
-        {users.map((user) =>{
-                return <RankingRow username={user.username} score={user.score}></RankingRow>
+
+        {usersData.map((user) =>{
+                return <RankingRow key={user.username} username={user.username} score={user.score}></RankingRow>
                 
             })}
         
@@ -31,6 +25,7 @@ export const RankingTable = () => {
 function RankingRow({username, score}) {
   return (
     <div className='flex flex-row gap-10 w-100 justify-center'>
+        
         <span className='w-40'>{username}</span>
         <span className='w-40'>{score}</span>
     </div>
